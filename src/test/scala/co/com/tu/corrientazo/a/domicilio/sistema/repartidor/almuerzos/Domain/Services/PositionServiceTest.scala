605068ea5c1f1330bdf290171cf67d063a259c5a
Jhon Edwin Zambrano Ortíz <jhonzaor@suramericana.com.co>
2019-01-11 22:32:16 -0500

    Se crean los test para el servicio de validación de la posición

---
+++ src/test/scala/co/com/tu/corrientazo/a/domicilio/sistema/repartidor/almuerzos/Domain/Services/PositionServiceTest.scala
@@ -0,0 +1,58 @@
+package co.com.tu.corrientazo.a.domicilio.sistema.repartidor.almuerzos.Domain.Services
+
+import cats.implicits._
+import co.com.tu.corrientazo.a.domicilio.sistema.repartidor.almuerzos.Application.Models.ErrorMessage
+import co.com.tu.corrientazo.a.domicilio.sistema.repartidor.almuerzos.Domain.Models.Position
+import co.com.tu.corrientazo.a.domicilio.sistema.repartidor.almuerzos.Domain.Types.NORTH
+import org.scalatest.{MustMatchers, WordSpecLike}
+
+class PositionServiceTest extends MustMatchers with WordSpecLike {
+
+  "validatePosition" should {
+
+    "When starts to validate the drone position " +
+      "Where X coordinate is outbound" +
+      "Then it must return an error " in {
+
+      val positiontoValidate = Position( 11, 5, NORTH )
+
+      val result = PositionService.validatePosition( positiontoValidate )
+
+      result mustBe ErrorMessage( "La posición en el eje X a la que desea moverse es inválida. " ).asLeft
+    }
+
+    "When starts to validate the drone position " +
+      "Where Y coordinate is outbound" +
+      "Then it must return an error " in {
+
+      val positiontoValidate = Position( 5, -11, NORTH )
+
+      val result = PositionService.validatePosition( positiontoValidate )
+
+      result mustBe ErrorMessage( "La posición en el eje Y a la que desea moverse es inválida. " ).asLeft
+    }
+
+    "When starts to validate the drone position " +
+      "Where X and Y coordinates are outbound" +
+      "Then it must return an error " in {
+
+      val positiontoValidate = Position( 15, -20, NORTH )
+
+      val result = PositionService.validatePosition( positiontoValidate )
+
+      result mustBe ErrorMessage( "La posición en el eje X a la que desea moverse es inválida. - La posición en el eje Y a la que desea moverse es inválida. " ).asLeft
+    }
+
+    "When starts to validate the drone position " +
+      "Where X and Y coordinates are inbound" +
+      "Then it must return an the validated position " in {
+
+      val positiontoValidate = Position( 10, 5, NORTH )
+
+      val result = PositionService.validatePosition( positiontoValidate )
+
+      result mustBe positiontoValidate.asRight
+    }
+  }
+
+}
